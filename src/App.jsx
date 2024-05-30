55d6efd64cb22efd70b0d23d131a6679d4dfcc0e
Your Name <you@example.com>
2024-05-29 22:06:32 -0300

    Comitando footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Componentes/Navbar/Navbar';
 import Login from './Componentes/Login/Login';
 import Cadastro from './Componentes/Cadastro/Cadastro';
+import Footer from './Componentes/Footer/Footer';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Footer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
